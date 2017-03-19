0e5b4a1f38b02fcd5131e8ad830c9a7bb0cfe5ec
Benedicte Raae <raae@bgraphic.no>
2017-03-19 14:15:17 +0100

    Done with lesson 13

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Header from './Header';
+import Fish from './Fish';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+  
+  constructor() {
+    super();
+    
+    this.addFish = this.addFish.bind(this);
+    
+    // Initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+  
+  addFish(fish) {
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    
+    this.setState({fishes: fishes});
+  }
+  
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+          <Fish />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render() {
+    return(
+      <p></p>
+    )
+  }
+}
+
+export default Order;
